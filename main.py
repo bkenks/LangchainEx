055611646bb11017a88134c91789276ff258ba87
bkenks <bkenks@gmail.com>
2023-12-25 16:59:24 -0500

    Completed agents example

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain.llms import OpenAI
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.agents.load_tools import get_all_tool_names
 
 def main():
     
     load_dotenv(find_dotenv())
-    llm = OpenAI(model_name="text-davinci-003")
+    davinciLLM = OpenAI(model_name="text-davinci-003")
 
-    try:
-        print(llm(input()))
-    except KeyboardInterrupt:
-        pass
+    tools = load_tools(["wikipedia", "llm-math"], llm=davinciLLM)
+    wikiAgent = initialize_agent(
+        tools, davinciLLM, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=1
+    )
+    
+    print(wikiAgent.run(
+        "In what year was python released and who is the original creator? Multiply the year by 3"
+    ))
 
 
 if __name__ == "__main__":
