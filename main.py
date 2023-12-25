780209600bd63e587315a78156d6d8a1d4cfdadd
bkenks <bkenks@gmail.com>
2023-12-25 16:07:31 -0500

    Completed demonstration of ConversationChain

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain.llms import OpenAI
+from langchain import ConversationChain
 
 def main():
     
     load_dotenv(find_dotenv())
     llm = OpenAI(model_name="text-davinci-003")
 
-    try:
-        print(llm(input()))
-    except KeyboardInterrupt:
-        pass
+    conversation = ConversationChain(llm=llm, verbose=1)
+
+    output = conversation.predict(input="Hi there!")
+    print(output)
+
+    output = conversation.predict(input="I'm doing well. What was the first thing I said to you just before this?")
+    print(output)
 
 
 if __name__ == "__main__":
