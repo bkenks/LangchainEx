2cb6964f71328626906319bf564d0564f46b895f
bkenks <bkenks@gmail.com>
2023-12-25 16:41:57 -0500

    Chains example completed

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
 def main():
     
     load_dotenv(find_dotenv())
     llm = OpenAI(model_name="text-davinci-003")
 
-    try:
-        print(llm(input()))
-    except KeyboardInterrupt:
-        pass
+    promptTemp = PromptTemplate(
+        input_variables=["product"],
+        template="What is a good name for a company that makes {product}?",
+    )
+
+    myChain = LLMChain(llm=llm, prompt=promptTemp)
+    print(myChain.run("AI Chatbots for a Law Office"))
 
 
 if __name__ == "__main__":
