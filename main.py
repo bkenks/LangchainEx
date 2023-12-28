bc05cebb2e4b7ec491d28460e1ead6b5a264fd22
bkenks <bkenks@gmail.com>
2023-12-28 13:33:06 -0500

    Added descriptive comments

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 
 def main():
     
     load_dotenv(find_dotenv())
-    llm = OpenAI(model_name="text-davinci-003")
+    llm = OpenAI()
+    chat_model = ChatOpenAI()
 
-    try:
-        print(llm(input()))
-    except KeyboardInterrupt:
-        pass
+    text = "What would be a good company name for a company that makes colorful socks?"
+    messages = [HumanMessage(content=text)]
+
+    # Uncomment to use LLM Text Model
+    #llm.invoke(text)
+
+    # Uncomment to use ChatModel
+    #chat_model.invoke(messages)
 
 
 if __name__ == "__main__":
