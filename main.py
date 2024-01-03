53a08dbde81a3a2f5e159e01b217cde6e2b15feb
bkenks <bkenks@gmail.com>
2024-01-02 21:31:33 -0500

    changes to main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv, find_dotenv
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 def main():
     
     load_dotenv(find_dotenv())
-    llm = OpenAI(model_name="text-davinci-003")
+    prompt = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
+    model = ChatOpenAI()
+    output_parser = StrOutputParser()
 
-    try:
-        print(llm(input()))
-    except KeyboardInterrupt:
-        pass
+    chain = prompt | model | output_parser
+
+    print(chain.invoke({"topic": "ice cream"}))
 
 
 if __name__ == "__main__":
