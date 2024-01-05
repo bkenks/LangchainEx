38353269c1c624216dcd49ab96294e2ff44d50c0
bkenks <bkenks@gmail.com>
2024-01-05 15:56:55 -0500

    completed example

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from dotenv import load_dotenv, find_dotenv
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
 def main():
     
-    # Variables and .env
     load_dotenv(find_dotenv())
-    llm = OpenAI(model_name="text-davinci-003")
 
-    prompt = PromptTemplate(
-        input_variables=["product"],
-        template="What is a good name for a company that makes {product}",
+    # Takes user-given context, embeds it, then stores it in a vector store
+    vectorStore = FAISS.from_texts(
+        ["harrison worked at kensho", "bears like to eat apples"],
+        embedding=OpenAIEmbeddings()
+        )
+    
+    # not really sure what this does
+    retreiver = vectorStore.as_retriever()
+
+    template = """Answer the question based only on the following context:
+    {context}
+    
+    Question: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatOpenAI()
+    outputParser = StrOutputParser()
+
+    setupAndRetreival = RunnableParallel(
+        {"context": retreiver, "question": RunnablePassthrough()}
     )
 
-    
-    print(llm(prompt.format(product="Smart Apps using Large Language Models (LLMs)")))
+    chain = setupAndRetreival | prompt | llm | outputParser
+
+    print(chain.invoke("Where did harrison work?"))
 
 
 if __name__ == "__main__":
